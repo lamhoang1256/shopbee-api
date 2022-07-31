08253b1c54b349d781de88b70c77b6323520403f
lamhoang1256 <lamhoang1256@gmail.com>
2022-07-31 18:59:05 +0700

    Update order controller

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -2,9 +2,9 @@ const orderControllers = require("../controllers/order.controller");
 const router = require("express").Router();
 
 router.post("/", orderControllers.createNewOrder);
-router.get("/", orderControllers.getAllOrderByUser);
+router.get("/user", orderControllers.getAllOrderByUser);
 router.get("/all", orderControllers.getAllOrderByAdmin);
-router.get("/:id", orderControllers.getSingleOrder);
+router.get("/", orderControllers.getSingleOrder);
 router.put("/:id/shipping", orderControllers.updateStatusShippingOfOrder);
 router.put("/:id/delivered", orderControllers.updateStatusDeliveredOfOrder);
 
