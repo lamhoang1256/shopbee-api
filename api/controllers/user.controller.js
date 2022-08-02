b7822e0bf1f213b6e181b6a79708d323b5d56783
lamhoang1256 <lamhoang1256@gmail.com>
2022-08-02 09:43:19 +0700

    Feat: add route change password of User

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 const { createError, responseSuccess } = require("../utils/response");
+const bcrypt = require("bcrypt");
 const User = require("../models/user.model");
 
 const userControllers = {
@@ -9,6 +10,27 @@ const userControllers = {
       const updatedUser = await user.updateOne({ $set: req.body });
       const response = {
         message: "Cập nhật thông tin thành công!",
+        data: updatedUser,
+      };
+      responseSuccess(res, response);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  changePassword: async (req, res, next) => {
+    try {
+      const userDB = await User.findById(req.body._id);
+      if (!userDB) return createError(404, "Không tìm thấy thông tin người dùng!");
+      if (!req.body.password && !req.body.newPassword)
+        return createError(404, "Không tìm thấy mật khẩu và mật khẩu mới!");
+      const validPassword = await bcrypt.compare(req.body.password, userDB.password);
+      if (!validPassword) return next(createError(422, "Mật khẩu hiện tại không đúng"));
+      const salt = await bcrypt.genSalt(10);
+      const hashed = await bcrypt.hash(req.body.newPassword, salt);
+      const updatedUser = await userDB.updateOne({ $set: { password: hashed } });
+      const response = {
+        message: "Đổi mật khẩu thành công!",
         data: updatedUser,
       };
       responseSuccess(res, response);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,5 +2,6 @@ const userControllers = require("../controllers/user.controller");
 const userRouter = require("express").Router();
 
 userRouter.put("/", userControllers.updateProfile);
+userRouter.put("/change-password", userControllers.changePassword);
 
 module.exports = userRouter;
