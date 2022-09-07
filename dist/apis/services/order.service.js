e28d5e28bf8d4dc54f74d59769403a0b0308961f
lamhoang1256 <lamhoang1256@gmail.com>
2022-09-07 09:59:06 +0700

    Chore: update order route

--- dist/apis/services/order.service.js
+++ dist/apis/services/order.service.js
@@ -56,7 +56,7 @@ const createNewOrder = (req) => __awaiter(void 0, void 0, void 0, function* () {
         const quantity = parseInt(orderItems[i].quantity);
         yield product_model_1.default.findOneAndUpdate({ _id: orderItems[i].product }, { $inc: { sold: quantity, stock: -quantity } });
     }
-    yield cart_model_1.default.deleteMany({ user: userId, product: { stock: { $gte: 0 } } });
+    yield cart_model_1.default.remove({ user: userId, product: { stock: { $gte: 0 } } });
     const firstProductOrder = yield product_model_1.default.findById(orderItems[0].product);
     const startTimeShipping = (0, helper_1.formatDateVN)(Date.now() + 3600 * 1000 * 48);
     const endTimeShipping = (0, helper_1.formatDateVN)(Date.now() + 3600 * 1000 * 96);
